897ee695290c3e15cdc9e7da69a6658d613fee7e
aliharby12 <aliharby565@gmail.com>
2025-05-09 01:07:25 +0300

    feat: add blog module with controller, service, and DTOs; integrate dotenv for configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
-import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { BlogModule } from './blog/blog.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { Blog } from './blog/entities/blog.entity';
 
 @Module({
   imports: [
-
-    UserModule],
+    ConfigModule.forRoot(),
+    UserModule,
+    BlogModule,
+    TypeOrmModule.forRoot({
+      type: (process.env.DB_TYPE || 'postgres') as 'postgres' | 'mysql' | 'mariadb' | 'sqlite' | 'mssql' | 'oracle',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [Blog],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
