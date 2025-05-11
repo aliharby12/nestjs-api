d26d0711ce681de40596162f7e3b3200ad2c946d
aliharby12 <aliharby565@gmail.com>
2025-05-11 18:10:48 +0300

    feat: add TypeORM configuration and migration scripts to package.json

---
+++ src/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const dataSourceOptions: DataSourceOptions = {
+    type: 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT as any || 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: ['dist/**/*.entity.js'],
+    migrations: ['dist/db/migrations/*.js'],
+    synchronize: false,
+    logging: true,
+    name: 'default',
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
