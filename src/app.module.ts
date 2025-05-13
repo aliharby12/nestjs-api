6f98e24a63e17ac54fe17061d0468af9d29ed08f
aliharby12 <aliharby565@gmail.com>
2025-05-13 11:48:57 +0300

    feat: enable TypeORM synchronization based on environment; set synchronize to true in development

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,7 +19,7 @@ import { ConfigModule } from '@nestjs/config';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_DATABASE,
       autoLoadEntities: true,
-      synchronize: false,
+      synchronize: process.env.NODE_ENV === 'development',
     }),
   ],
   controllers: [AppController],
